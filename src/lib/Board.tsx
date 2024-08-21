e036632d94806a01ccff98792601c141728445fc
generic-name-2166 <kirbesson277@mail.ru>
2024-08-21 19:58:27 +0300

    feat: projectiles

--- src/lib/Board.tsx
+++ src/lib/Board.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { MouseEventHandler, useEffect, useRef, type JSX } from "react";
-import { clear, Hero } from "./board.ts";
+import { clear, Hero, Projectile } from "./board.ts";
 /* import styles from "./Board.module.scss"; */
 
 export default function Board(): JSX.Element {
@@ -33,8 +33,10 @@ export default function Board(): JSX.Element {
 
     let mounted = true;
 
-    const hero1 = new Hero(10, 20, 10, 2, 100, true);
-    const hero2 = new Hero(90, 20, 10, 2, 100, false);
+    const hero1 = new Hero(10, 20, 10, 100, 2, 300, 150, true);
+    const hero2 = new Hero(190, 20, 10, 100, 2, 300, 150, false);
+    let projectiles1: Projectile[] = [];
+    let projectiles2: Projectile[] = [];
 
     const loop = () => {
       if (!mounted) {
@@ -42,10 +44,37 @@ export default function Board(): JSX.Element {
       }
       clear(ctx, width, height);
 
-      hero1.tick(mouse.current.x, mouse.current.y);
+      const proj1 = hero1.tick(mouse.current.x, mouse.current.y);
       hero1.draw(ctx);
-      hero2.tick(mouse.current.x, mouse.current.y);
+      if (projectiles1.length > 10) {
+        projectiles1.shift();
+      }
+      if (proj1 !== null) {
+        projectiles1.push(proj1);
+      }
+
+      const proj2 = hero2.tick(mouse.current.x, mouse.current.y);
       hero2.draw(ctx);
+      if (projectiles2.length > 10) {
+        projectiles2.shift();
+      }
+      if (proj2 !== null) {
+        projectiles2.push(proj2);
+      }
+
+      // side effects in filter :(
+      projectiles1 = projectiles1.filter(
+        (proj) => !proj.tick(hero2.x, hero2.y, hero2.radius),
+      );
+      projectiles2 = projectiles2.filter(
+        (proj) => !proj.tick(hero1.x, hero1.y, hero1.radius),
+      );
+      for (const proj of projectiles1) {
+        proj.draw(ctx);
+      }
+      for (const proj of projectiles2) {
+        proj.draw(ctx);
+      }
 
       requestAnimationFrame(loop);
     };
--- src/lib/board.ts
+++ src/lib/board.ts
@@ -1,30 +1,79 @@
+const MOUSE_RADIUS = 5;
+const PROJECTILE_VELOCITY = 5;
+const PROJECTILE_RADIUS = 3;
+
 function detectCollision(
   x1: number,
   y1: number,
   x2: number,
   y2: number,
-  radius: number,
+  radius1: number,
+  radius2: number,
 ): boolean {
-  return radius >= Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return radius1 + radius2 >= Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+export class Projectile {
+  constructor(
+    private x: number,
+    private readonly y: number,
+    private readonly velocity: number,
+    private readonly radius: number,
+    private readonly width: number,
+  ) {}
+
+  /**
+   * @returns whether projectile has been destroyed on this tick
+   */
+  tick(heroX: number, heroY: number, heroRadius: number): boolean {
+    const maybe = this.x + this.velocity;
+    if (
+      detectCollision(this.x, this.y, heroX, heroY, this.radius, heroRadius)
+    ) {
+      // hero takes damage
+      return true;
+    } else if (maybe + this.radius >= this.width || maybe - this.radius <= 0) {
+      // Projectile reached the board boundary
+      return true;
+    }
+    this.x = maybe;
+    return false;
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.beginPath();
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
 }
 
 export class Hero {
+  private cooldown: number = 0;
+
   constructor(
-    private readonly x: number,
-    private y: number,
-    private readonly radius: number,
+    public readonly x: number,
+    public y: number,
+    public readonly radius: number,
+    /** the lower, the faster hero shoots */
+    private firerate: number,
     private velocity: number,
+    private readonly width: number,
     private readonly height: number,
-    // whether the hero is facing right
+    /** whether the hero is facing right */
     private readonly direction: boolean,
   ) {}
 
-  tick(mouseX: number, mouseY: number): void {
+  /**
+   * @returns a new projectile if it fires one, otherwise null
+   */
+  tick(mouseX: number, mouseY: number): Projectile | null {
     const maybe = this.y + this.velocity;
     if (
       maybe + this.radius >= this.height ||
       maybe - this.radius <= 0 ||
-      detectCollision(this.x, this.y, mouseX, mouseY, this.radius)
+      detectCollision(this.x, this.y, mouseX, mouseY, this.radius, MOUSE_RADIUS)
     ) {
       // Change direction on boundary
       // or on mouse collision
@@ -32,6 +81,23 @@ export class Hero {
     }
 
     this.y = this.y + this.velocity;
+
+    if (this.cooldown >= this.firerate) {
+      this.cooldown = 0;
+      const projVelocity = this.direction
+        ? PROJECTILE_VELOCITY
+        : -PROJECTILE_VELOCITY;
+      return new Projectile(
+        this.x,
+        this.y,
+        projVelocity,
+        PROJECTILE_RADIUS,
+        this.width,
+      );
+    } else {
+      this.cooldown += 1;
+      return null;
+    }
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
