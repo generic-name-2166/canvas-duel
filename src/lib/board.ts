85022220119526b2c3b8bb3e484cfb32d73cd779
generic-name-2166 <kirbesson277@mail.ru>
2024-08-22 01:50:47 +0300

    feat: scoreboard

--- src/lib/board.ts
+++ src/lib/board.ts
@@ -2,6 +2,7 @@ const MOUSE_RADIUS = 5;
 const PROJECTILE_VELOCITY = 5;
 const PROJECTILE_RADIUS = 3;
 const MAX_PROJECTILES = 1000;
+const SCORE_OFFSET = 60;
 
 function detectCollision(
   x1: number,
@@ -14,69 +15,10 @@ function detectCollision(
   return radius1 + radius2 >= Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
 }
 
-export class BoardManager {
-  private hero1: Hero;
-  private projectiles1: Projectile[] = [];
-  private hero2: Hero;
-  private projectiles2: Projectile[] = [];
-
-  constructor(
-    private ctx: CanvasRenderingContext2D,
-    width: number,
-  ) {
-    // Saving clean canvas to reset to on every tick
-    this.ctx.save();
-    this.hero1 = new Hero(50, 20, 10, 100, 2, true);
-    this.hero2 = new Hero(width - 50, 20, 10, 100, 2, false);
-  }
-
-  tick(
-    time: DOMHighResTimeStamp,
-    width: number,
-    height: number,
-    mouseX: number,
-    mouseY: number,
-  ): void {
-    const proj1 = this.hero1.tick(time, mouseX, mouseY, height);
-    if (this.projectiles1.length >= MAX_PROJECTILES) {
-      this.projectiles1.shift();
-    }
-    if (proj1 !== null) {
-      this.projectiles1.push(proj1);
-    }
-
-    const proj2 = this.hero2.tick(time, mouseX, mouseY, height);
-    if (this.projectiles2.length >= MAX_PROJECTILES) {
-      this.projectiles2.shift();
-    }
-    if (proj2 !== null) {
-      this.projectiles2.push(proj2);
-    }
-
-    // side effects in filter :(
-    this.projectiles1 = this.projectiles1.filter(
-      (proj) =>
-        !proj.tick(this.hero2.x, this.hero2.y, this.hero2.radius, width),
-    );
-    this.projectiles2 = this.projectiles2.filter(
-      (proj) =>
-        !proj.tick(this.hero1.x, this.hero1.y, this.hero1.radius, width),
-    );
-  }
-
-  draw(): void {
-    this.ctx.restore();
-
-    this.hero1.draw(this.ctx);
-    this.hero2.draw(this.ctx);
-
-    for (const proj of this.projectiles1) {
-      proj.draw(this.ctx);
-    }
-    for (const proj of this.projectiles2) {
-      proj.draw(this.ctx);
-    }
-  }
+const enum ProjectileResult {
+  None,
+  Hit,
+  Miss,
 }
 
 class Projectile {
@@ -87,27 +29,24 @@ class Projectile {
     private readonly radius: number,
   ) {}
 
-  /**
-   * @returns whether projectile has been destroyed on this tick
-   */
   tick(
     heroX: number,
     heroY: number,
     heroRadius: number,
     width: number,
-  ): boolean {
+  ): ProjectileResult {
     const maybe = this.x + this.velocity;
     if (
       detectCollision(this.x, this.y, heroX, heroY, this.radius, heroRadius)
     ) {
       // hero takes damage
-      return true;
+      return ProjectileResult.Hit;
     } else if (maybe + this.radius >= width || maybe - this.radius <= 0) {
       // Projectile reached the board boundary
-      return true;
+      return ProjectileResult.Miss;
     }
     this.x = maybe;
-    return false;
+    return ProjectileResult.None;
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
@@ -120,8 +59,10 @@ class Projectile {
 }
 
 class Hero {
-  /* milliseconds since last shot */
+  /** milliseconds since last shot */
   private lastShot: DOMHighResTimeStamp = 0;
+  /** number of times hero hit the target */
+  public hits: number = 0;
 
   constructor(
     public x: number,
@@ -176,5 +117,107 @@ class Hero {
     ctx.lineWidth = 3;
     ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
     ctx.stroke();
+
+    const score = `${this.hits} hits`;
+    ctx.font = "48px serif";
+    if (this.direction) {
+      ctx.fillText(score, this.x + SCORE_OFFSET, SCORE_OFFSET);
+      return;
+    }
+    const offset = ctx.measureText(score).width;
+    ctx.fillText(score, this.x - offset - SCORE_OFFSET, SCORE_OFFSET);
+  }
+}
+
+export class BoardManager {
+  private hero1: Hero;
+  private projectiles1: Projectile[] = [];
+  private hero2: Hero;
+  private projectiles2: Projectile[] = [];
+
+  constructor(
+    private ctx: CanvasRenderingContext2D,
+    width: number,
+  ) {
+    // Saving clean canvas to reset to on every tick
+    this.ctx.save();
+    this.hero1 = new Hero(50, 20, 10, 100, 2, true);
+    this.hero2 = new Hero(width - 50, 20, 10, 100, 2, false);
+  }
+
+  tick(
+    time: DOMHighResTimeStamp,
+    width: number,
+    height: number,
+    mouseX: number,
+    mouseY: number,
+  ): void {
+    this.hero2.x = width - 50;
+
+    const proj1 = this.hero1.tick(time, mouseX, mouseY, height);
+    if (this.projectiles1.length >= MAX_PROJECTILES) {
+      this.projectiles1.shift();
+    }
+    if (proj1 !== null) {
+      this.projectiles1.push(proj1);
+    }
+
+    const proj2 = this.hero2.tick(time, mouseX, mouseY, height);
+    if (this.projectiles2.length >= MAX_PROJECTILES) {
+      this.projectiles2.shift();
+    }
+    if (proj2 !== null) {
+      this.projectiles2.push(proj2);
+    }
+
+    // side effects in filter :(
+    this.projectiles1 = this.projectiles1.filter((proj) => {
+      const result = proj.tick(
+        this.hero2.x,
+        this.hero2.y,
+        this.hero2.radius,
+        width,
+      );
+      switch (result) {
+        case ProjectileResult.None:
+          return true;
+        case ProjectileResult.Hit:
+          this.hero1.hits += 1;
+          return false;
+        case ProjectileResult.Miss:
+          return false;
+      }
+    });
+    this.projectiles2 = this.projectiles2.filter((proj) => {
+      const result = proj.tick(
+        this.hero1.x,
+        this.hero1.y,
+        this.hero1.radius,
+        width,
+      );
+      switch (result) {
+        case ProjectileResult.None:
+          return true;
+        case ProjectileResult.Hit:
+          this.hero2.hits += 1;
+          return false;
+        case ProjectileResult.Miss:
+          return false;
+      }
+    });
+  }
+
+  draw(): void {
+    this.ctx.restore();
+
+    this.hero1.draw(this.ctx);
+    this.hero2.draw(this.ctx);
+
+    for (const proj of this.projectiles1) {
+      proj.draw(this.ctx);
+    }
+    for (const proj of this.projectiles2) {
+      proj.draw(this.ctx);
+    }
   }
 }
